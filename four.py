0a52fafd3aabd456b14fa537050997833ea91fb9
琦佳 杨 <yangqijia0423@163.com>
2023-05-22 12:11:58 +0000

    SAVE

---
+++ four.py
@@ -0,0 +1,11 @@
+#22、s = "ajldjlajfdljfddd"，去重并从小到大排序输出"adfjl"
+#set去重，去重转成list,利用sort方法排序，reeverse=False是从小到大排
+#list是不 变数据类型，s.sort时候没有返回值，所以注释的代码写法不正确
+
+s = "ajldjlajfdljfddd"
+s = set(s)
+s = list(s)
+s.sort(reverse=False)
+# s = s.sort(reverse=False)
+res = "".join(s)
+print(res)
